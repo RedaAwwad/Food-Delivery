8432d87531660ad0efa9ab5e31e26076685fb847
Ahmed Omran <aomran564.4@gmail.com>
2025-12-04 18:59:06 +0200

    delete types.d.ts

--- src/utils/errors/types.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated This file is deprecated. Please import from './types' instead.
- * This file is kept for backward compatibility only.
- */
-export type { ErrorCode, ErrorDetails, ErrorFormat, ErrorResponse } from "./types";
