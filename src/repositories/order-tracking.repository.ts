7b9cc147d186422ea67fb90f8f5e1e880681d78a
Ahmed Omran <aomran564.4@gmail.com>
2026-01-14 13:16:41 +0200

    create order tracking repo

---
+++ src/repositories/order-tracking.repository.ts
@@ -0,0 +1,31 @@
+import { prisma } from "../config/prisma.config";
+import { NotFoundError } from "../utils/errors";
+
+class OrderTrackingRepository {
+  async getOrderTrackingStatus(orderId: string, customerId: string) {
+  const tracking = await prisma.orderTracking.findUnique({
+    where: { orderId_customerId: { orderId, customerId } },
+    select: { orderId: true, customerId: true, trackingStatus: true }
+  });
+
+  if (!tracking) throw NotFoundError("Order tracking not found");
+
+  return tracking
+}
+
+ async updateOrderTrackingStatus(orderId:string , customerId:string, trackingStatus:TrackingStatusStep[]) {
+   return await prisma.orderTracking.update({
+    where: {
+      orderId_customerId: {
+        orderId,
+        customerId
+      },
+    },
+    data: {
+      trackingStatus,
+    },
+  });
+  
+ }
+}
+export const orderTrackingRepository = new OrderTrackingRepository();
