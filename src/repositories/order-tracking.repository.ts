cb02acfdd0238d12f3edd80c55052ffd50187c99
Ahmed Omran <aomran564.4@gmail.com>
2026-01-14 13:27:08 +0200

    add order tracking dtos

--- src/repositories/order-tracking.repository.ts
+++ src/repositories/order-tracking.repository.ts
@@ -1,32 +1,32 @@
 import { prisma } from "../config/prisma.config";
-import { TrackingStatusStep } from "../dto/orderTrackingStatus";
+import { TrackingStatusStep } from "../dto/orderTrackingStatus.dto";
 import { NotFoundError } from "../utils/errors";
 
 class OrderTrackingRepository {
   async getOrderTrackingStatus(orderId: string, customerId: string) {
-  const tracking = await prisma.orderTracking.findUnique({
-    where: { orderId_customerId: { orderId, customerId } },
-    select: { orderId: true, customerId: true, trackingStatus: true }
-  });
+    const tracking = await prisma.orderTracking.findUnique({
+      where: { orderId_customerId: { orderId, customerId } },
+      select: { orderId: true, customerId: true, trackingStatus: true }
+    });
 
-  if (!tracking) throw NotFoundError("Order tracking not found");
+    if (!tracking) throw NotFoundError("Order tracking not found");
 
-  return tracking
-}
+    return tracking
+  }
 
- async updateOrderTrackingStatus(orderId:string , customerId:string, trackingStatus:TrackingStatusStep[]) {
-   return await prisma.orderTracking.update({
-    where: {
-      orderId_customerId: {
-        orderId,
-        customerId
+  async updateOrderTrackingStatus(orderId: string, customerId: string, trackingStatus: TrackingStatusStep[]) {
+    return await prisma.orderTracking.update({
+      where: {
+        orderId_customerId: {
+          orderId,
+          customerId
+        },
       },
-    },
-    data: {
-      trackingStatus,
-    },
-  });
-  
- }
+      data: {
+        trackingStatus,
+      },
+    });
+
+  }
 }
 export const orderTrackingRepository = new OrderTrackingRepository();
