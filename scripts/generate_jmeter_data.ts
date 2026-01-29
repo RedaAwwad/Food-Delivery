9a2c7c9aa89cf2b969079d9e944a435339a14f25
Ahmed Omran <aomran564.4@gmail.com>
2026-01-29 02:59:19 +0200

    finish upgrading to Prisma 7

--- scripts/generate_jmeter_data.ts
+++ scripts/generate_jmeter_data.ts
@@ -3,6 +3,11 @@ import { prisma } from "../src/config/prisma.config";
 import * as fs from "fs";
 import * as path from "path";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 async function main() {
     console.log("Generating JMeter data...");
 
--- src/config/prisma.config.ts
+++ src/config/prisma.config.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
+  adapter,
   log: ["query", "error"],
 });
