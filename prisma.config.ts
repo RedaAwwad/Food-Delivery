38a79c571815cad3ba4b680e9d8d6f1a2a76d07b
Ahmed Omran <aomran564.4@gmail.com>
2026-02-04 17:48:44 +0200

    feat/role-management FINISH#2

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "@prisma/config";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
+
+const DATABASE_USER = process.env.DB_USER;
+const DATABASE_PASSWORD = process.env.DB_PASSWORD;
+const DATABASE_LOCAL_PORT = process.env.DB_LOCAL_PORT;
+const DATABASE_NAME = process.env.DB_NAME;
+
+
+const DATABASE_URL = `postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:${DATABASE_LOCAL_PORT}/${DATABASE_NAME}?schema=public`
+
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
@@ -9,6 +17,6 @@ export default defineConfig({
         seed: "tsx prisma/seed.ts",
     },
     datasource: {
-        url: process.env.DATABASE_URL,
+        url: DATABASE_URL,
     },
 });
